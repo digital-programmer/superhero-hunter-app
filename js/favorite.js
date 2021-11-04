58928abd3aef177bf81c76a2161abb7c3e2ead9c
digital-programmer <mohandibya123@gmail.com>
2021-11-04 23:26:46 +0530

    Add comments

--- js/favorite.js
+++ js/favorite.js
@@ -1,1 +1,75 @@
-var fav_heroes, fav_list = window.localStorage.getItem("fav_hero_list"), no_fav = document.getElementById("no-fav"), character_display = document.getElementById("display_characters"); function getData(b) { return new Promise((c, a) => { fetch(b).then(a => a.json()).then(a => { c(a) }).catch(b => { a(b) }) }) } function loadCharacters(b) { let c = []; b.forEach(a => { if ("" != a) { c.push(getData(`https://api.allorigins.win/get?url=https://superheroapi.com/api/4301778239934401/${a}`)) } }), Promise.all(c).then(a => { displayFavHeroes(a) }) } function displayFavHeroes(b) { var c; no_fav.style.display = "none"; var d = []; b.forEach(b => { var c = JSON.parse(b.contents); d.push(`<li class="hero-list-item flex" id="${c.id}"><div class="list-item-img" alt="${c.name}"><a href="./hero.html#${c.id}"><img src="${c.image.url}"></a></div><div class="detail-container valign flex space-between"><div class="list-item-details"<p class="list-item-hero-name">${c.name}</p><p class="list-item-hero-real-name">${c.biography["full-name"]}</p></div><div class="add-favorite"><img src="./images/delete_black_24dp.svg" class="delete-btn" alt="delete favorite"></div></div><div class="show_details flex halign"><a href="./hero.html#${c.id}"> Show details -></a></div></li>`) }), c = d.join(""), character_display.innerHTML = c, loadEventListeners() } function loadEventListeners() { document.querySelectorAll(".delete-btn").forEach(a => { a.addEventListener("click", b => { b.stopPropagation(); var c = b.target.parentNode.parentNode.parentNode.getAttribute("id"); const a = fav_heroes.indexOf(c); if (-1 < a) { if (fav_heroes.splice(a, 1), document.getElementById(c).remove(), 0 == fav_heroes.length) no_fav.style.display = "block"; else if (1 == fav_heroes.length) { var d = fav_heroes[0]; "" == d && (no_fav.style.display = "block") } window.localStorage.setItem("fav_hero_list", fav_heroes) } }) }) } null == fav_list ? no_fav.style.display = "block" : (fav_heroes = fav_list.split(","), fav_heroes = [...new Set(fav_heroes)]), null == fav_list || 0 == fav_list.length ? no_fav.style.display = "block" : (no_fav.style.display = "none", loadCharacters(fav_heroes));
+var fav_heroes, // variable to store favorite heroes
+    fav_list = window.localStorage.getItem("fav_hero_list"),  // variable to store local storage
+    no_fav = document.getElementById("no-fav"), // error message selector
+    character_display = document.getElementById("display_characters"); // favorite character display
+
+
+// get data of requested id from url
+function getData(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((result) => {
+                resolve(result);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+}
+
+
+// take all liked characters from localStorage and load their data from their IDs
+function loadCharacters(hero_ids) {
+    let results = [];
+    hero_ids.forEach((id) => {
+        if ("" != id) {
+            results.push(getData(`https://api.allorigins.win/get?url=https://superheroapi.com/api/4301778239934401/${id}`));
+        }
+    }),
+        Promise.all(results).then((res) => {
+            displayFavHeroes(res);
+        });
+}
+
+// after loading all data in an array, display each character
+function displayFavHeroes(hero_data) {
+    var data;
+    no_fav.style.display = "none";
+    var html_list = [];
+    hero_data.forEach((item) => {
+        var data = JSON.parse(item.contents);
+        html_list.push(
+            `<li class="hero-list-item flex" id="${data.id}"><div class="list-item-img" alt="${data.name}"><a href="./hero.html#${data.id}"><img src="${data.image.url}"></a></div><div class="detail-container valign flex space-between"><div class="list-item-details"<p class="list-item-hero-name">${data.name}</p><p class="list-item-hero-real-name">${data.biography["full-name"]}</p></div><div class="add-favorite"><img src="./images/delete_black_24dp.svg" class="delete-btn" alt="delete favorite"></div></div><div class="show_details flex halign"><a href="./hero.html#${data.id}"> Show details -></a></div></li>`
+        );
+    });
+    var html_to_be_added = html_list.join("");
+    character_display.innerHTML = html_to_be_added;
+
+    // after displaying DOM, add addEventListener for delete button
+    loadEventListeners();
+}
+
+
+// After displaying fav heroes, add event listeners to their associated delete button 
+function loadEventListeners() {
+    document.querySelectorAll(".delete-btn").forEach((item) => {
+        item.addEventListener("click", (event) => {
+            event.stopPropagation();
+            var target_id = event.target.parentNode.parentNode.parentNode.getAttribute("id");
+            const index = fav_heroes.indexOf(target_id);
+            if (-1 < index) {
+                if ((fav_heroes.splice(index, 1), document.getElementById(target_id).remove(), 0 == fav_heroes.length)) no_fav.style.display = "block";
+                else if (1 == fav_heroes.length) {
+                    var d = fav_heroes[0];
+                    "" == d && (no_fav.style.display = "block");
+                }
+                window.localStorage.setItem("fav_hero_list", fav_heroes);
+            }
+        });
+    });
+}
+
+// when the page loads, take the list of ids stored in local storage in form of key-value pair, value is list of hero-ids. make it unique and make requests for all ids
+null == fav_list ? (no_fav.style.display = "block") : ((fav_heroes = fav_list.split(",")), (fav_heroes = [...new Set(fav_heroes)])),
+    null == fav_list || 0 == fav_list.length ? (no_fav.style.display = "block") : ((no_fav.style.display = "none"), loadCharacters(fav_heroes));
--- js/home.js
+++ js/home.js
@@ -1,1 +1,116 @@
-var searchBar = document.getElementById("searchBar"), clearBtn = document.getElementById("clear"), message = document.getElementById("not-found"), characterList = document.getElementById("characterList"); let heroCharacters = []; var characters; let favoriteHeroString = window.localStorage.getItem("fav_hero_list"); var favoriteHero; favoriteHero = null === favoriteHeroString ? [] : favoriteHeroString.split(","); var controller = new AbortController; function displayNotFound() { characterList.innerHTML = "", message.style.display = "block" } function displayCharacters(e) { message.style.display = "none"; var t = [], a = window.localStorage.getItem("fav_hero_list"); a = null != a ? a.split(",") : [], e.forEach(function (e) { var r; void 0 !== a.find(t => t == e.id) ? (r = `<li class="hero-list-item flex" id="${e.id}"><div class="list-item-img"><a href="./hero.html#${e.id}"><img src="${e.image.url}" alt="${e.name}"></a></div><div class="detail-container valign flex space-between"><div class="list-item-details"><p class="list-item-hero-name">${e.name}</p><p class="list-item-hero-real-name">${e.biography["full-name"]}</p></div><div class="add-favorite"><img src="./images/299063_heart_icon.svg" class="like-btn" alt="add favorite"></div></div><div class="show_details flex halign"><a href="./hero.html#${e.id}"> Show details -></a></div></li>`, t.push(r)) : (r = `<li class="hero-list-item flex" id="${e.id}"><div class="list-item-img"><a href="./hero.html#${e.id}"><img src="${e.image.url}" alt="${e.name}"></a></div><div class="detail-container valign flex space-between"><div class="list-item-details"><p class="list-item-hero-name">${e.name}</p><p class="list-item-hero-real-name">${e.biography["full-name"]}</p></div><div class="add-favorite"><img src="./images/favorite_border_black_24dp.svg" class="like-btn" alt="add favorite"></div></div><div class="show_details flex halign"><a href="./hero.html#${e.id}"> Show details -></a></div></li>`, t.push(r)) }); var r = t.join(""); characterList.innerHTML = r } searchBar.addEventListener("input", function () { "" != this.value ? clearBtn.style.display = "block" : clearBtn.style.display = "none" }), searchBar.addEventListener("keyup", e => { const t = e.target.value; controller.abort(), controller = new AbortController, "" == t ? (clearBtn.style.display = "none", characterList.innerHTML = "") : loadCharacters(t).then(e => { "error" == (characters = JSON.parse(e.contents)).response ? displayNotFound() : (displayCharacters(heroCharacters = characters.results), document.querySelectorAll(".like-btn").forEach(e => { e.addEventListener("click", e => { e.stopPropagation(); var t = e.target, a = t.parentNode.parentNode.parentNode.getAttribute("id"); if ("./images/favorite_border_black_24dp.svg" == t.getAttribute("src")) t.setAttribute("src", "./images/299063_heart_icon.svg"), favoriteHero.push(a), window.localStorage.setItem("fav_hero_list", favoriteHero); else { t.setAttribute("src", "./images/favorite_border_black_24dp.svg"); const e = favoriteHero.indexOf(a); e > -1 && favoriteHero.splice(e, 1), window.localStorage.setItem("fav_hero_list", favoriteHero) } }) })) }).catch(e => { displayNotFound() }) }), clearBtn.addEventListener("click", function () { searchBar.value = "", clearBtn.style.display = "none", searchBar.focus(), characterList.innerHTML = "" }); const loadCharacters = e => { return getHeroData(`https://api.allorigins.win/get?url=https://superheroapi.com/api/4301778239934401/search/${e}`).then(e => e).catch(e => { console.log("Character not found") }) }; function getHeroData(e) { return new Promise((t, a) => { fetch(e).then(e => e.json()).then(e => { t(e) }).catch(e => { a(e) }) }) }
+// Selectors
+var searchBar = document.getElementById("searchBar"), // searchbar
+    clearBtn = document.getElementById("clear"), // searchbar clear button
+    message = document.getElementById("not-found"), // error message when hero not found
+    characterList = document.getElementById("characterList"); // display character container
+
+let heroCharacters = []; // hero characters found after search
+var characters;
+
+// check if some ids are present in local storage
+let favoriteHeroString = window.localStorage.getItem("fav_hero_list");
+var favoriteHero;
+
+// if present, make a list of ids else return empty list
+favoriteHero = null === favoriteHeroString ? [] : favoriteHeroString.split(",");
+
+// abort request if new request made subsequently
+var controller = new AbortController();
+
+// if the search string is not found
+function displayNotFound() {
+    (characterList.innerHTML = ""), (message.style.display = "block");
+}
+
+// display all characters found after searching the string
+function displayCharacters(e) {
+    // remove error message
+    message.style.display = "none";
+
+    var t = [],
+        a = window.localStorage.getItem("fav_hero_list"); // get all ids from local storage
+    (a = null != a ? a.split(",") : []),
+        // for every element in search result
+        e.forEach(function (e) {
+            var r;
+            // check if id is present in search result, if present display result with filled heart; if not marked as favorite before, display the card with outlined heart
+            void 0 !== a.find((t) => t == e.id)
+                ? ((r = `<li class="hero-list-item flex" id="${e.id}"><div class="list-item-img"><a href="./hero.html#${e.id}"><img src="${e.image.url}" alt="${e.name}"></a></div><div class="detail-container valign flex space-between"><div class="list-item-details"><p class="list-item-hero-name">${e.name}</p><p class="list-item-hero-real-name">${e.biography["full-name"]}</p></div><div class="add-favorite"><img src="./images/299063_heart_icon.svg" class="like-btn" alt="add favorite"></div></div><div class="show_details flex halign"><a href="./hero.html#${e.id}"> Show details -></a></div></li>`),
+                    t.push(r))
+                : ((r = `<li class="hero-list-item flex" id="${e.id}"><div class="list-item-img"><a href="./hero.html#${e.id}"><img src="${e.image.url}" alt="${e.name}"></a></div><div class="detail-container valign flex space-between"><div class="list-item-details"><p class="list-item-hero-name">${e.name}</p><p class="list-item-hero-real-name">${e.biography["full-name"]}</p></div><div class="add-favorite"><img src="./images/favorite_border_black_24dp.svg" class="like-btn" alt="add favorite"></div></div><div class="show_details flex halign"><a href="./hero.html#${e.id}"> Show details -></a></div></li>`),
+                    t.push(r));
+        });
+    // prepare the html elements and join to form a single string
+    var r = t.join("");
+    characterList.innerHTML = r; // view on screen
+}
+
+
+// show clear button only when some thing is typed
+searchBar.addEventListener("input", function () {
+    "" != this.value ? (clearBtn.style.display = "block") : (clearBtn.style.display = "none");
+});
+
+
+// when something is entered, immediately search for the string
+searchBar.addEventListener("keyup", (e) => {
+    const t = e.target.value; // get target string value
+    controller.abort(), // reject previous requests, if made
+        (controller = new AbortController()), // for new request
+        "" == t
+            ? ((clearBtn.style.display = "none"), (characterList.innerHTML = "")) // if search string is empty
+            : loadCharacters(t)  // else load the characters from search string
+                .then((e) => {
+                    "error" == (characters = JSON.parse(e.contents)).response
+                        ? displayNotFound() // if characters not found, show error message
+                        : (displayCharacters((heroCharacters = characters.results)), // else display the characters
+                            document.querySelectorAll(".like-btn").forEach((e) => {
+                                e.addEventListener("click", (e) => {  // add event listeners for add to wishlist button
+                                    e.stopPropagation();
+                                    var t = e.target,
+                                        a = t.parentNode.parentNode.parentNode.getAttribute("id");
+                                    // if liked, add the id to local storage
+                                    if ("./images/favorite_border_black_24dp.svg" == t.getAttribute("src"))
+                                        t.setAttribute("src", "./images/299063_heart_icon.svg"), favoriteHero.push(a), window.localStorage.setItem("fav_hero_list", favoriteHero);
+                                    // else toggle the heart button and remove element from the local storage
+                                    else {
+                                        t.setAttribute("src", "./images/favorite_border_black_24dp.svg");
+                                        const e = favoriteHero.indexOf(a);
+                                        e > -1 && favoriteHero.splice(e, 1), window.localStorage.setItem("fav_hero_list", favoriteHero);
+                                    }
+                                });
+                            }));
+                })
+                .catch((e) => {
+                    displayNotFound(); // if err, display error message
+                });
+});
+
+
+// when clicked, clear the text in search bar
+clearBtn.addEventListener("click", function () {
+    (searchBar.value = ""), (clearBtn.style.display = "none"), searchBar.focus(), (characterList.innerHTML = "");
+});
+
+// load characters and make API calls to get data
+const loadCharacters = (searchString) => {
+    return getHeroData(`https://api.allorigins.win/get?url=https://superheroapi.com/api/4301778239934401/search/${searchString}`)
+        .then((results) => results)
+        .catch((err) => {
+            console.log("Character not found");
+        });
+};
+
+// send request and return promise as data of 
+function getHeroData(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((results) => {
+                resolve(results);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+}
